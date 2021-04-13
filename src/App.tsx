a06681a79a8c7a8ed2e334c14480bfcad405df8f
Alegherix <psao69@gmail.com>
2021-04-13 13:33:22 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from '@react-google-maps/api';
+import { Libraries } from '@react-google-maps/api/dist/utils/make-load-script-url';
+import mapStyles from './utils/mapstyles';
+import styled from 'styled-components';
+import MushromSVG from './components/svg/Mushroom';
+
+const libraries: Libraries = ['places'];
+const mapContainerStyle = {
+  width: '100vvw',
+  height: '100vh',
+};
+const center = {
+  lat: 57.652,
+  lng: 12.116,
+};
+const options = {
+  styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl: true,
+};
+
+const Title = styled.h1`
+  font-size: 3rem;
+  text-align: center;
+  color: #9e70db;
+  display: inline;
+`;
+
+const LogoContainer = styled.div`
+  z-index: 2;
+  position: absolute;
+  left: 3%;
+  top: 3%;
+  height: 40px;
+  width: 40px;
+`;
 
 function App() {
+  console.log(process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
+
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY as string,
+    libraries,
+  });
+
+  if (loadError) return <div>Error loading...</div>;
+  if (!isLoaded) return <div>Loading...</div>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <LogoContainer>
+        <Title>Forager</Title>
+        <MushromSVG />
+      </LogoContainer>
+
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={14}
+        center={center}
+        options={options}
+      ></GoogleMap>
+    </main>
   );
 }
 
